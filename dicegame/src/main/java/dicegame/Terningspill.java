fd5a418ca8c427318f1b1305eb5f752bfcd1bff6
Simen Østensen <simen.ostensen@gmail.com>
2019-02-12 11:32:01 +0100

    :construction: Terningspill.java

--- dicegame/src/main/java/dicegame/Terningspill.java
+++ dicegame/src/main/java/dicegame/Terningspill.java
@@ -3,12 +3,13 @@ package dicegame;
 /**
  * Hello world!
  */
-public final class App {
-    private App() {
+public final class Terningspill {
+    private Terningspill() {
     }
 
     /**
      * Says hello to the world.
+     * 
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
