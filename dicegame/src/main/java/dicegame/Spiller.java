cf697236df1398f221f497c8163d8ede00a77063
Morten Helland <mortenhelland@outlook.com>
2019-02-12 11:20:48 +0100

    Getters and setters

--- dicegame/src/main/java/dicegame/Spiller.java
+++ dicegame/src/main/java/dicegame/Spiller.java
@@ -6,6 +6,42 @@ public class Spiller {
     private int verdi;
 
 
+    public Spiller (String navn, int verdi) {
+this.navn=navn;
+this.verdi=verdi;
+
+    }
+
+    /**
+     * @return the navn
+     */
+    public String getNavn() {
+        return navn;
+    }
+
+    /**
+     * @param navn the navn to set
+     */
+    public void setNavn(String navn) {
+        this.navn = navn;
+    }
+
+    /**
+     * @return the verdi
+     */
+    public int getVerdi() {
+        return verdi;
+    }
+
+    /**
+     * @param verdi the verdi to set
+     */
+    public void setVerdi(int verdi) {
+        this.verdi = verdi;
+    }
+
+    
+
     
 
 } 
