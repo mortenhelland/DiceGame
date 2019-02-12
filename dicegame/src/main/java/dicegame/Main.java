558e87b912cc83a86418947ceae768aab251d9d5
180312 <31240925+180312@users.noreply.github.com>
2019-02-12 12:21:58 +0100

    Added main

---
+++ dicegame/src/main/java/dicegame/Main.java
@@ -0,0 +1,10 @@
+package dicegame;
+
+public class Main {
+
+	/** 
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+			Terningspill terningspill = new Terningspill();
+		}
