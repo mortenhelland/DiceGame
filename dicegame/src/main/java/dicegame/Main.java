87f371acc0c98ea3dbbbdb06e4c657563419c61c
Simen Østensen <simen.ostensen@gmail.com>
2019-02-12 12:26:08 +0100

    Main

--- dicegame/src/main/java/dicegame/Main.java
+++ dicegame/src/main/java/dicegame/Main.java
@@ -2,9 +2,11 @@ package dicegame;
 
 public class Main {
 
-	/** 
+    /**
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-			Terningspill terningspill = new Terningspill();
-		}
+        Terningspill terningspill = new Terningspill();
+
+    }
+}
