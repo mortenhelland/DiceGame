0cccae8b817f22be3011f366bb714284e73a3176
Simen Østensen <simen.ostensen@gmail.com>
2019-02-12 12:24:13 +0100

    :construction: Terningspill

--- dicegame/src/main/java/dicegame/Terningspill.java
+++ dicegame/src/main/java/dicegame/Terningspill.java
@@ -6,20 +6,6 @@ import java.util.Scanner;
  * Hello world!
  */
 public final class Terningspill {
-    private Terningspill() {
-    }
-
-    /**
-     * Says hello to the world.
-     * 
-     * @param args The arguments of the program.
-     */
-    public static void main(String[] args) {
-
-        Kopp kopp = new Kopp();
-        String a = kopp.trill();
-        System.out.println(a);
-    }
 
     public void leggTilSpiller() {
         Spiller[] spiller = null;
