b2c3ecfef3d33530fba139a953333749364fb6b6
Anders Mæhlum Halvorsen <andersmhalvorsen@gmail.com>
2019-02-20 14:45:58 +0100

    Update Terningspill.java

--- dicegame/src/main/java/dicegame/Terningspill.java
+++ dicegame/src/main/java/dicegame/Terningspill.java
@@ -1,28 +1,44 @@
 package dicegame;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Terningspill {
 
     int id;
+    ArrayList<Spiller> players;
 
-    public void leggTilSpiller() {
-       
+public Terningspill(int id){
+    this.id = id;
+    players = new ArrayList<Spiller>();
+}
+
+    public void leggTilSpiller(String navn) {
+        Spiller p1 = new Spiller(navn,0);
+        players.add(p1);
+      
     }
 
     public void spill() {
 
         Scanner scanner = new Scanner(System.in);
+        int antallSpillere = 0;
+
         System.out.println("Hvor mange spillere skal være med?");
         String input = scanner.nextLine();
 
-        int antallSpillere = 0;
+        
 
         if(input != null && input.length() == 1){
                 antallSpillere = Integer.parseInt(input);
-                
+
         } else {
             return;
+        }
+
+        for(int i = 0; i < antallSpillere; i++){
+            String navn = scanner.nextLine();
+            //leggTilSpiller(navn);
         }
 
 
