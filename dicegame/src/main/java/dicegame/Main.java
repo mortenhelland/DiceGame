23a00e61a01fa447252e5dea922bfb1cea299b04
Anders Mæhlum Halvorsen <andersmhalvorsen@gmail.com>
2019-02-20 14:01:13 +0100

    Test

--- dicegame/src/main/java/dicegame/Main.java
+++ dicegame/src/main/java/dicegame/Main.java
@@ -7,7 +7,8 @@ public class Main {
      */
     public static void main(String[] args) {
         Terningspill terningspill = new Terningspill();
-
-    
+        Kopp kopp = new Kopp();
+        System.out.println(kopp.trill());    
+        System.out.println(kopp.getSum());
     }
 }
