290c5decf5f827aae8a559f43337a287705e07f3
Simen Østensen <simen.ostensen@gmail.com>
2019-02-20 13:17:57 +0100

    :construction: LeggTilSpiler()
    Prøvde meg på noen ting her

--- dicegame/src/main/java/dicegame/Terningspill.java
+++ dicegame/src/main/java/dicegame/Terningspill.java
@@ -1,5 +1,6 @@
 package dicegame;
 
+import java.lang.reflect.AnnotatedArrayType;
 import java.util.Scanner;
 
 public class Terningspill {
@@ -7,6 +8,33 @@ public class Terningspill {
     public void leggTilSpiller() {
         Spiller[] spiller = null;
         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hvor mange spillere skal være med?");
+        String input = scanner.nextLine();
+
+        int antSpillere = 0;
+
+        if (input != null && input.length() == 1) {
+            try {
+                antSpillere = Integer.parseInt(input);
+                if (antSpillere < 2 || antSpillere > 5) {
+                    System.out.println("Ugyldig antall spillere");
+                }
+            } catch (NumberFormatException e) {
+                antSpillere = 0;
+            }
+        }
+        while (antSpillere < 2 || antSpillere > 5) {
+            spiller = new Spiller[antSpillere];
+
+            for (int i = 0; i < antSpillere; i++) {
+                System.out.println("Navnet på spiller " + (i + 1));
+                String spillerNavn = scanner.nextLine();
+                spiller[i] = new Spiller(spillerNavn);
+            }
+
+        }
+
     }
 
     public void spill() {
